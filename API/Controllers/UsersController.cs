50138a6d395140717f8ca84ac8b583452819f8da
DH <doyeon8952@gmail.com>
2020-11-29 11:36:22 -0600

    section3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
