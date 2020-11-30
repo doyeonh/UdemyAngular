06f1b28a53d42f24fb646444f6e80c288086b29c
DH <doyeon8952@gmail.com>
2020-11-29 21:45:46 -0600

    Done Ssection4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+ 
+
+    }
+}
